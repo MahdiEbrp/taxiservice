ea1352a8ec73bf164ba029d17ef4533945ca3f38
MahdiEbrp <mahdi.a.ebrp@gmail.com>
2022-08-11 12:22:49 +0430

    add bing map

---
+++ lib/FetchData.ts
@@ -0,0 +1,93 @@
+import axios, { AxiosError } from 'axios';
+export interface SigninResult {
+    error: string | undefined;
+    status: number;
+    ok: boolean;
+    url: string | null;
+}
+const timeout = 10000;
+export const PostData = async (url: string, data: any) => {
+    try {
+        const response = await axios.post(url, JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            timeout: timeout,
+        });
+        return response;
+
+    }
+    catch (err) {
+        if (err instanceof AxiosError) {
+            return err.response;
+        }
+        else
+            return null;
+    }
+};
+export const GetData = async (url: string) => {
+    try {
+        const response = await axios.get(url,{timeout: timeout});
+        return response;
+
+    }
+    catch (err) {
+        if (err instanceof AxiosError) {
+            return err.response;
+        }
+        else
+            return null;
+    }
+};
+export const PutData = async (url: string, data: any) => {
+    try {
+        const response = await axios.put(url, JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            timeout: timeout,
+        });
+        return response;
+
+    }
+    catch (err) {
+        if (err instanceof AxiosError) {
+            return err.response;
+        }
+        else
+            return null;
+    }
+};
+export const DeleteData = async (url: string) => {
+    try {
+        const response = await axios.delete(url);
+        return response;
+
+    }
+    catch (err) {
+        if (err instanceof AxiosError) {
+            return err.response;
+        }
+        else
+            return null;
+    }
+};
+export const PatchData = async (url: string, data: any) => {
+    try {
+        const response = await axios.patch(url, JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            timeout: timeout,
+        });
+        return response;
+
+    }
+    catch (err) {
+        if (err instanceof AxiosError) {
+            return err.response;
+        }
+        else
+            return null;
+    }
+};
