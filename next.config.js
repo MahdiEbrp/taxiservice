79763126fb4a898824353e737963b601e5d9b196
MahdiEbrp <mahdi.a.ebrp@gmail.com>
2022-09-07 01:45:43 +0430

    add axiom

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withAxiom } = require('next-axiom');
+
+const nextConfig = withAxiom({
   reactStrictMode: false,
   i18n: {
-    locales: ['en','en-US', 'fa','fa-IR'],
+    locales: ['en', 'en-US', 'fa', 'fa-IR'],
     defaultLocale: 'en',
   },
-};
+});
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import MainLayout from '../components/MainLayout';
 
+export { reportWebVitals } from 'next-axiom';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <MainLayout><Component {...pageProps} /></MainLayout>;
 }
