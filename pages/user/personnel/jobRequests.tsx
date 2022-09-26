19afecb1105baba9202aaa565c80d59a26bd6ad1
MahdiEbrp <mahdi.a.ebrp@gmail.com>
2022-09-26 10:50:30 +0330

    fix react name

--- pages/user/personnel/jobRequests.tsx
+++ pages/user/personnel/jobRequests.tsx
@@ -3,19 +3,28 @@ import Head from 'next/head';
 import AuthorizedLayout from '../../../components/AuthorizedLayout';
 import { LanguageContext } from '../../../components/context/LanguageContext';
 import { useContext } from 'react';
+import Paper from '@mui/material/Paper';
+import CenterBox from '../../../components/controls/CenterBox';
 
-const jobRequests: NextPage = () => {
+const JobRequests: NextPage = () => {
 
     const { language } = useContext(LanguageContext);
-    const { jobRequestsPage } = language;
+    const { jobRequestsPage, settings } = language;
 
     return (
         <AuthorizedLayout>
-            <Head>
-                <title>{jobRequestsPage.title}</title>
-            </Head>
+            <>
+                <Head>
+                    <title>{jobRequestsPage.title}</title>
+                </Head>
+                <Paper dir={settings.direction}>
+                    <CenterBox>
+                        <h1>hello world</h1>
+                    </CenterBox>
+                </Paper>
+            </>
         </AuthorizedLayout>
     );
 };
 
-export default jobRequests;
+export default JobRequests;
